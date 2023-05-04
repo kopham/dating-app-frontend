84904a2462d4dbc2d01594f1f3f431b812aca21e
kopham <khoi.2.pham@ucdenver.edu>
2023-05-03 23:18:57 -0600

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,10 @@
 import React from 'react'
 import './Header.css'
 
-import AccountCircleIcon from '@mui/icons-material/AccountCircle'; // ==> nay la cai icon người
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'; 
+import IconButton from '@mui/material/IconButton';  
+import ForumIcon from '@mui/icons-material/Forum';  
 
-import IconButton from '@mui/material/IconButton';  // này là chọn phương thức bấm (vì xài cách "nhấn để chọn" trêm icon --> nên dùng cái này)
-
-import ForumIcon from '@mui/icons-material/Forum';  // ==> này là cái icon khung chat bên phải
-
-// tiêu đề: anh nghĩ ý là dòng đầu tiên của trang web mình 
-// trước tiên: quy định cái phương thức dùng icon : --> click on button --> nên xài IconButton
-// cái nào khai báo trước là cái icon đầu tiên (AccountCircleIcon) --> đến cái img (cái react icon) --> xong cái ForumIcon 
 const Header = () => {
     return (
         <div className="header">
